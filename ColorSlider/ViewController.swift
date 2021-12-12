f8d6a81738675bfb07b0b6104d62e99953611ebe
Anna <annasmelekhina@gmail.com>
2021-12-12 19:08:15 +0300

    added sliders slider values show in labels

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var valueRed: UILabel!
+    @IBOutlet var valueGreen: UILabel!
+    @IBOutlet var valueBlue: UILabel!
+    
+    @IBOutlet var sliderRedValue: UISlider!
+    @IBOutlet var sliderGreenValue: UISlider!
+    @IBOutlet var sliderBlueValue: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        
+        valueRed.text = String(sliderRedValue.value)
+        valueBlue.text = String(sliderBlueValue.value)
+        valueGreen.text = String(sliderGreenValue.value)
+        
+        
+        
+        
     }
 
+    @IBAction func sliderRed() {
+        valueRed.text = String(sliderRedValue.value)
+    }
 
+    @IBAction func sliderGreen() {
+        valueGreen.text = String(sliderGreenValue.value)
+    }
+    
+    @IBAction func sliderBlue() {
+        valueBlue.text = String(sliderBlueValue.value)
+    }
+    
 }
 
